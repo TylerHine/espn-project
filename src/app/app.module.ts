d194499ff0d130a43a5e6003b6311a8b740ffa72
Tyler <tjh0064@mix.wvu.edu>
2022-01-25 18:28:02 -0500

    Changes made to ESPN project.

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { EventsNavbarComponent } from './header/eventsnavbar.component';
+import { MainNavbarComponent } from './header/mainnavbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EventsNavbarComponent,
+    MainNavbarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
