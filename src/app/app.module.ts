5390743ac0e77971e67b4c139cc6c8cf8ad67534
Tyler <tjh0064@mix.wvu.edu>
2022-02-01 18:18:33 -0500

    Added a footer component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SideCardComponent } from './body/sidecard.component';
 import { FooterComponent } from './footer/footer.component';
 import { EventsNavbarComponent } from './header/eventsnavbar.component';
 import { MainNavbarComponent } from './header/mainnavbar.component';
@@ -11,7 +12,8 @@ import { MainNavbarComponent } from './header/mainnavbar.component';
     AppComponent,
     EventsNavbarComponent,
     MainNavbarComponent,
-    FooterComponent
+    FooterComponent,
+    SideCardComponent
   ],
   imports: [
     BrowserModule
