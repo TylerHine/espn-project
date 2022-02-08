cadb558e0c6f3580ecd6e876ba200313aa6c427c
Tyler <tjh0064@mix.wvu.edu>
2022-02-08 18:14:23 -0500

    Added CSS style urls for eventsnavbar.ts.

--- src/app/header/eventsnavbar.component.ts
+++ src/app/header/eventsnavbar.component.ts
@@ -3,7 +3,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-eventsnavbar',
-    templateUrl: 'eventsnavbar.component.html'
+    templateUrl: 'eventsnavbar.component.html',
+    styleUrls: ['eventsnavbar.component.css']
 })
 export class EventsNavbarComponent {
 
