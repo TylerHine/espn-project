4b158d1d4ee69c264ce5304dfe2760b30e7c2712
Tyler <tjh0064@mix.wvu.edu>
2022-02-08 18:15:29 -0500

    Added CSS style urls to mainnavbar.ts file.

--- src/app/header/mainnavbar.component.ts
+++ src/app/header/mainnavbar.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-mainnavbar',
-    templateUrl: 'mainnavbar.component.html'
+    templateUrl: 'mainnavbar.component.html',
+    styleUrls: ['mainnavbar.component.css']
   
 })
 
