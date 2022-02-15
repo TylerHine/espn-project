826fd17fd4dcd936d22fed2a5cad5dec2c3218e0
Tyler <tjh0064@mix.wvu.edu>
2022-02-15 18:05:38 -0500

    Added ts class for sidecard component.

---
+++ src/app/body/leftcard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core"
+
+@Component ({
+    selector: "app-leftcard",
+    templateUrl: "leftcard.component.html",
+    styleUrls: ['leftcard.component.css']
+})
+
+export class LeftCardComponent {
+
+}
