1752f48b3e57950836d1d48d8ebbfe3a7f1a6e66
Tyler <tjh0064@mix.wvu.edu>
2022-02-15 18:06:53 -0500

    Added ts file for footer.

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core"
+import { Component, Input } from "@angular/core"
 
 @Component({
     selector: 'app-footer',
@@ -7,4 +7,11 @@ import { Component } from "@angular/core"
 })
 export class FooterComponent{
 
+    @Input() img: string;
+    @Input() ufc: string;
+
+    constructor() {
+        this.img = "";
+        this.ufc = "";
+    }
 }
