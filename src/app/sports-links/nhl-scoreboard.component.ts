1464f26a296aae5630b07dca6b13a345ab27ae66
Tyler <tjh0064@mix.wvu.edu>
2022-02-22 18:07:04 -0500

    Added nhl layout component classes for routing

---
+++ src/app/sports-links/nhl-scoreboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-nhl-scoreboard',
+    templateUrl: 'nhl-scoreboard.component.html'
+})
+
+
+
+export class NhlScoreboardComponent {
+
+}
