1b479f9bde19af9f206275a87f51a50fa92d31d7
Tyler <tjh0064@mix.wvu.edu>
2022-02-22 18:07:25 -0500

    Added soccer layout components and image

---
+++ src/app/sports-links/soccer-scoreboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-soccer-scoreboard',
+    templateUrl: 'soccer-scoreboard.component.html'
+})
+
+
+
+export class SoccerScoreboardComponent {
+
+}
