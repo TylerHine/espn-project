94f52e2bd892ec8e2afd399d6764992362ee9cc7
Tyler <tjh0064@mix.wvu.edu>
2022-02-22 18:06:43 -0500

    added nfl layout component classes for routing

---
+++ src/app/sports-links/nfl-scoreboard.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-nfl-scoreboard',
+    templateUrl: 'nfl-scoreboard.component.html',
+    styleUrls: ['nfl-scoreboard.component.css']
+})
+
+
+
+export class NflScoreboardComponent {
+
+}
