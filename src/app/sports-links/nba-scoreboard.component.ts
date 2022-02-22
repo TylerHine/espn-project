5327d9c199ca477e4ee2ddd8ec0614037771ac04
Tyler <tjh0064@mix.wvu.edu>
2022-02-22 18:06:24 -0500

    type script for nba scoreboard layout component

---
+++ src/app/sports-links/nba-scoreboard.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-nba-scoreboard',
+    templateUrl: 'nba-scoreboard.component.html',
+    styleUrls: ['nba-scoreboard.component.css']
+})
+
+
+
+export class NbaScoreboardComponent {
+
+}
