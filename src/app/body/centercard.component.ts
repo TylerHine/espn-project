7702bfcc042a370d01088b462964c3568636d0be
Tyler <tjh0064@mix.wvu.edu>
2022-02-22 18:05:09 -0500

    Added center card type script class

---
+++ src/app/body/centercard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core"
+
+@Component ({
+    selector: "app-centercard",
+    templateUrl: "centercard.component.html",
+    styleUrls: ['centercard.component.css']
+})
+
+export class CenterCardComponent {
+
+}
