a3d9389ca181dd7917a25271b73b8b3e46338136
Tyler <tjh0064@mix.wvu.edu>
2022-03-09 12:06:49 -0500

    Added product item model class with constructor.

---
+++ src/app/body/product-item.model.ts
@@ -0,0 +1,10 @@
+export class ProductItemModel {
+    img: string;
+    title: string;
+
+
+    constructor(img: string, title: string) {
+        this.img = img;
+        this.title = title;
+    }
+}
