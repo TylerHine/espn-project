9579db4e33de961c69db0d9e4944cd1662ca4aa1
Tyler <tjh0064@mix.wvu.edu>
2022-03-09 12:05:18 -0500

    Added mock list to app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { mock_product_list } from './body/mock_product_list';
+import { ProductItemModel } from './body/product-item.model';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,14 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'espn';
+  product: ProductItemModel [] = [];
+
+  constructor() {
+
+    for(var product of mock_product_list) {
+      console.log(product);
+      this.product.push(product);
+    }
+    // put some code to fetch data from backend using HTTP
+  }
 }
